314fb5b2b3f1e6e4f8d81dfe95c14f98d02f9bec
ruben-qb <ruben@qburst.com>
2016-07-09 16:03:37 +0530

    fixes

--- Postgre/db.sh
+++ Postgre/db.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 DATABASE=docker
 USERNAME=docker
-HOSTNAME=172.17.0.2
+HOSTNAME=127.0.0.1
 export PGPASSWORD=docker
 
 psql -h $HOSTNAME -U $USERNAME $DATABaSE << EOF
-CREATE TABLE airport_freq(
+CREATE TABLE airport_frequency(
   id    integer,
   airport_ref integer,
-  airport_ident integer,
+  airport_ident varchar,
   type varchar,
   description varchar,
-  frequency_mhz varchar,
+  frequency_mhz varchar
 );
-copy airport_freq(id,airport_ref,airport_ident,type,description,frequency_mhz) FROM '/home/airport-frequencies.csv' DELIMITER ',' CSV HEADER;
+copy airport_frequency(id,airport_ref,airport_ident,type,description,frequency_mhz) FROM '/home/airport-frequencies.csv' DELIMITER ',' CSV HEADER;
 EOF
 
